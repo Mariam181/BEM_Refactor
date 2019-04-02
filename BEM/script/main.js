8b6994714624165ce42ed9f524abb5926e650a0b
Mariam181 <39503602+Mariam181@users.noreply.github.com>
2019-04-02 17:19:53 -0600

    quiz#3 con BEM

---
+++ BEM/script/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    
+});
+
+$(function(){
+    $('#slider div:gt(0)').hide();
+        setInterval(function(){
+            $('#slider div:first-child').fadeOut(1000)
+                .next('div').fadeIn(1000)
+                .end().appendTo('#slider');
+        },3000);
+});
